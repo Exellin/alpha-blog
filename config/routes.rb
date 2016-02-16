1a713aea84019c4f56a5767960b41e4bab76db6e
Exellin <carnegieshawn@yahoo.ca>
2016-02-16 20:07:27 +0000

    changed path to homepage

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  root 'welcome#home'
+  get 'about', to: 'welcome#about'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
   
-  get 'welcome/home', to: 'welcome#home'
-  get 'welcome/about', to: 'welcome#about'
+  #get 'about', to: 'pages#about'
+
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
