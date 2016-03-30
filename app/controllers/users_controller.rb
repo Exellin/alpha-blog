c83c9d75237715d6e9bd7dd209bd1265e1cdd3b8
Exellin <carnegieshawn@yahoo.ca>
2016-03-30 19:21:15 +0000

    add users listing and show who created article

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   
+  def index
+    @users = User.all
+  end
+  
   def new
     @user = User.new
   end
@@ -38,3 +42,5 @@ class UsersController < ApplicationController
   end
   
 end
+
+
